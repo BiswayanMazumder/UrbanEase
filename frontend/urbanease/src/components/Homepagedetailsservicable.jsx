6b56999ce26df0b3a18b21b9af7d1957bf1e77f9
BiswayanMazumder <biswayanmazumder27@gmail.com>
2026-04-18 19:12:52 +0530

    Servicable area homepage #2

--- frontend/urbanease/src/components/Homepagedetailsservicable.jsx
+++ frontend/urbanease/src/components/Homepagedetailsservicable.jsx
@@ -6,6 +6,17 @@ export default function Homepagedetailsservicable() {
             <div className="vjcghdsbv">
                 <div className="ggddjgndbvds">
                     <h2>Home services at your <br></br> doorstep</h2>
+                    <div className="dshfjdhksjdvd">
+                        <div className="ehevfhgfjf">
+                            <p>What are you looking for?</p>
+                            <div className="hegbehdbesf">
+                                <div className="dhjdbjdnj">
+                                    <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1774526047861-554660.jpeg" alt="" style={{borderRadius:"10px"}}/>
+                                    <p>Women's Salon & Spa</p>
+                            </div>
+                                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="ehsdbsd">
