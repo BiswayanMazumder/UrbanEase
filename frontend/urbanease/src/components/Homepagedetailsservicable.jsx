cc730c22d3242bb1c892f89e7003236eadc87338
BiswayanMazumder <biswayanmazumder27@gmail.com>
2026-04-18 22:02:07 +0530

    Updating serviceable are page #3

--- frontend/urbanease/src/components/Homepagedetailsservicable.jsx
+++ frontend/urbanease/src/components/Homepagedetailsservicable.jsx
@@ -1,28 +1,55 @@
 import "../styles/Homepageservicable.css";
 export default function Homepagedetailsservicable() {
-  return (
-   <div className="servicablehomepage">
-        <div className="werfgfdfdsckfj">
-            <div className="vjcghdsbv">
-                <div className="ggddjgndbvds">
-                    <h2>Home services at your <br></br> doorstep</h2>
-                    <div className="dshfjdhksjdvd">
-                        <div className="ehevfhgfjf">
-                            <p>What are you looking for?</p>
-                            <div className="hegbehdbesf">
-                                <div className="dhjdbjdnj">
-                                    <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1774526047861-554660.jpeg" alt="" style={{borderRadius:"10px"}}/>
-                                    <p>Women's Salon & Spa</p>
-                            </div>
+    return (
+        <div className="servicablehomepage">
+            <div className="werfgfdfdsckfj">
+                <div className="vjcghdsbv">
+                    <div className="ggddjgndbvds">
+                        <h2>Home services at your <br></br> doorstep</h2>
+                        <div className="dshfjdhksjdvd">
+                            <div className="ehevfhgfjf">
+                                <p>What are you looking for?</p>
+                                <div className="hegbehdbesf">
+                                    <div className="dhjdbjdnj">
+                                        <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1774526047861-554660.jpeg" alt="" />
+                                        <p>Women's Salon & Spa</p>
+                                    </div>
+
+                                    <div className="dhjdbjdnj">
+                                        <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1774526691081-afedc4.jpeg" alt="" />
+                                        <p>Salon Prime</p>
+                                    </div>
+
+                                    <div className="dhjdbjdnj">
+                                        <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1681711961404-75dfec.jpeg" alt="" />
+                                        <p>Cleaning</p>
+                                    </div>
+
+                                    <div className="dhjdbjdnj">
+                                        <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1768544313670-e3f84b.jpeg" alt="" />
+                                        <p>AC & Appliance Repair</p>
+                                    </div>
+
+                                    <div className="dhjdbjdnj">
+                                        <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_233,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/supply/customer-app-supply/1752476639421-112dfa.jpeg" alt="" />
+                                        <p>Native Water Purifier</p>
+                                    </div>
+
+                                    <div className="dhjdbjdnj">
+                                        <img src="https://www.urbancompany.com/img?bucket=urbanclap-prod&quality=90&format=auto/w_56,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1674120935535-f8d5c8.jpeg" alt="" />
+                                        <p>Home Painting</p>
+                                    </div>
                                 </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="ehsdbsd">
-                <img src="https://www.urbancompany.com/img?quality=90&format=auto/dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1696852847761-574450.jpeg" alt="" width="90%" height="100%" />
+                <div className="ehsdbsd">
+                    <img src="https://www.urbancompany.com/img?quality=90&format=auto/dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1696852847761-574450.jpeg" alt="" width="90%" height="100%" />
+                </div>
+
             </div>
         </div>
-   </div>
-  );
+    );
 }
