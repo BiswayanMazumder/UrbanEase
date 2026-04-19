5db1eb9ef2e1e15d3dfa6bac1cc6e4a79040856d
BiswayanMazumder <biswayanmazumder27@gmail.com>
2026-04-19 15:38:07 +0530

    Creating backend #3

--- frontend/urbanease/src/components/Homepagedetailsservicable.jsx
+++ frontend/urbanease/src/components/Homepagedetailsservicable.jsx
@@ -4,7 +4,7 @@ export default function Homepagedetailsservicable() {
     const [services, setServices] = useState([]);
     const [newAndNoteworthy, setNewAndNoteworthy] = useState([]);
     useEffect(() => {
-        fetch("http://127.0.0.1:8000/api/most-booked")
+        fetch("/api/most-booked")
             .then((res) => res.json())
             .then((data) => {
                 console.log("API response:", data);
@@ -15,7 +15,7 @@ export default function Homepagedetailsservicable() {
     }, []);
     
     useEffect(() => {
-        fetch("http://127.0.0.1:8000/api/new-and-noteworthy")
+        fetch("/api/new-and-noteworthy")
             .then((res) => res.json())
             .then((data) => {
                 console.log("API response new and noteworthy:", data);
